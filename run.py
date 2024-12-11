53d6954d5205bb5e276055786f360d30ddfdd738
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-12-11 14:59:13 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-from app import app
+from flask import Flask, render_template
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Crear una instancia de Flask
+app = Flask(__name__)
 
+# Definir las rutas
+@app.route('/')
+def index():
+    # Renderiza la vista index.html
+    return render_template('usuarios/index.html')
+
+# Arrancar el servidor cuando se ejecuta este archivo directamente
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
