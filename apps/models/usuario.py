50ebcdaa6934c16364629f7f036d9d13e359e323
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-12-11 15:27:30 -0600

    Update usuario.py

--- apps/models/usuario.py
+++ apps/models/usuario.py
@@ -1,9 +1,16 @@
-class Usuario:
-    def __init__(self, id, usuario, contrasena):
-        self.id = id
-        self.usuario = usuario
-        self.contrasena = contrasena
+# apps/models/usuario.py
+import mysql.connector
 
-    def to_dict(self):
-        """Convierte el objeto a un diccionario para facilitar su uso en la vista"""
-        return {"id": self.id, "usuario": self.usuario, "contrasena": self.contrasena}
+def get_db_connection():
+    # Configuración de conexión MySQL
+    try:
+        con = mysql.connector.connect(
+            host="185.232.14.52",  # Cambia a tu host
+            database="u760464709_tst_sep",  # Cambia a tu base de datos
+            user="u760464709_tst_sep_usr",  # Cambia a tu usuario
+            password="dJ0CIAFF="  # Cambia a tu contraseña
+        )
+        return con
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
